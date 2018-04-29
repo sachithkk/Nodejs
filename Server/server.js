95e452acd1feb74352b849495da1e88ec3f53add
sachith tharaka <tharakakk@gmail.com>
2018-04-29 14:04:26 +0530

    this is commited sachith

--- Server/server.js
+++ Server/server.js
@@ -1,31 +1,30 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// create model.
-var Todo = mongoose.model('Todo', {
-  text:{
-    type:String
-  },
-  completed:{
-    type:Boolean
-  },
-  completedAt:{
-    type:Number
-  }
+// Local libries
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos' , (req, res) => {
+
+  var todo = new Todo({
+    text:req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+
 });
 
-
-// set values
-var newTodo = new Todo({
-  text: "Cook dinner"
-});
-
-
-// save into database
-newTodo.save().then((doc) => {
-  console.log("Save data !");
-}, (err) => {
-  console.log("Unable to save data ",err);
+app.listen(3000, () => {
+  console.log("Started on port 3000");
 });
